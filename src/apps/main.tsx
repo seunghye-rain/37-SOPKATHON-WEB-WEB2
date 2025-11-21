044495cbc30ea0165bc0bd5623d3e3232e6729b9
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-22 00:40:01 +0900

    [feat] 절대 경로 설정

--- src/apps/main.tsx
+++ src/apps/main.tsx
@@ -1,8 +1,7 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-
-import { QueryProvider } from '../shared/query/query-provider.tsx'; // todo: 절대경로 변경
-import App from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "@/apps/App.tsx";
+import { QueryProvider } from '@/shared/query/query-provider.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
+<<<<<<< HEAD
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from "path";
+>>>>>>> c793f43 ([feat] 절대 경로 설정)
 
 export default defineConfig({
+<<<<<<< HEAD
   plugins: [react(), svgr()],
 });
+=======
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
+>>>>>>> c793f43 ([feat] 절대 경로 설정)
