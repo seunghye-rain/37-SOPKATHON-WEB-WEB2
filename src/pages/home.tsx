7dade90d586edc359fed354018f8715b807f2b85
yooncandooit <dotsi0@dgu.ac.kr>
2025-11-23 05:01:13 +0900

    remove: 테스트 코드 제거

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,10 +1,7 @@
-import MatchingForm from '@/widgets/matching-form/matching-form';
-
 const Home = () => {
   return (
     <>
       <h1>홈 화면입니다</h1>
-      <MatchingForm onSubmit={(data) => console.log(data)} />
     </>
   );
 };
