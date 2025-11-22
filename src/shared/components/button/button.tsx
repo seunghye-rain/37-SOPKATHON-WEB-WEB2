91873d0901b304744ff0ca57bb1a0e375be1111d
JS MacBook Pro <j_seokpark@naver.com>
2025-11-23 03:01:28 +0900

    refactor: 코드리뷰 반영

--- src/shared/components/button/button.tsx
+++ src/shared/components/button/button.tsx
@@ -1,34 +1,18 @@
 import type { RecipeVariants } from '@vanilla-extract/recipes';
-import type { ReactNode } from 'react';
+import type { ButtonHTMLAttributes, ReactNode } from 'react';
 
 import { buttonStyle } from './button.css';
 
-type ButtonStyleProps = RecipeVariants<typeof buttonStyle>;
+type ButtonStyleVariants = RecipeVariants<typeof buttonStyle>;
 
-type ButtonProps = ButtonStyleProps & {
-  children: ReactNode;
-  disabled?: boolean;
-  onClick?: () => void;
-  type?: 'button' | 'submit' | 'reset';
-  size?: string;
-  selected?: boolean;
-};
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  ButtonStyleVariants & {
+    children: ReactNode;
+  };
 
-const Button = ({
-  children,
-  disabled,
-  onClick,
-  type = 'button',
-  size,
-  selected,
-}: ButtonProps) => {
+const Button = ({ children, size, selected, ...props }: ButtonProps) => {
   return (
-    <button
-      type={type}
-      disabled={disabled}
-      onClick={onClick}
-      className={buttonStyle({ size, selected })}
-    >
+    <button {...props} className={buttonStyle({ size, selected })}>
       {children}
     </button>
   );
